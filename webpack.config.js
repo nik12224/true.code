a4624ed3299ae777c084b46a8b5bf3bce69c2cbb
Zaur <0lezgin5@gmail.com>
2023-12-09 22:35:04 +0500

    Создание проекта, верстка главной и формы

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const config = {
+	mode: 'production',
+	entry: {
+		index: './src/js/index.js'
+		// contacts: './src/js/contacts.js',
+		// about: './src/js/about.js',
+	},
+	output: {
+		filename: '[name].bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file-loader',
+				options: {
+				  name: '[name].[ext]'
+				}
+			  },
+		],
+	},
+};
+
+module.exports = config;
